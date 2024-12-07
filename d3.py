d3a9e76f268b2daa8fa48b3f494a8e37c66c53d6
Nic0las2001 <nicodami19@gmail.com>
2024-12-07 17:57:59 +0000

    d3 combined

--- d3.py
+++ d3.py
@@ -5,27 +5,19 @@ import re
 input = open("d3.txt","r")
 
 sum = 0
-part = 2
+part = 1
 do = True
 
-if part == 1:
-    for line in input:
-        for i in range(len(line)):
-            if re.match(r"mul\(\d+,\d+\)", line[i:]):
-                first_bracket = line[i:].index(")") + i
-                vals = re.findall(r"\d+", line[i:first_bracket])
+for line in input:
+    for i in range(len(line)):
+        if re.match(r"mul\(\d+,\d+\)", line[i:]):
+            first_bracket = line[i:].index(")") + i
+            vals = re.findall(r"\d+", line[i:first_bracket])
+            if do:
                 sum += int(vals[0]) * int(vals[1])
-else:
-    for line in input:
-        for i in range(len(line)):
-            if re.match(r"mul\(\d+,\d+\)", line[i:]):
-                first_bracket = line[i:].index(")") + i
-                vals = re.findall(r"\d+", line[i:first_bracket])
-                if do:
-                    sum += int(vals[0]) * int(vals[1])
-            elif re.match(r"do\(\)",line[i:]):
-                do = True
-            elif re.match("don\'t\(\)",line[i:]): 
-                do = False
+        elif re.match(r"do\(\)",line[i:]) and part == 2:
+            do = True
+        elif re.match("don\'t\(\)",line[i:]) and part == 2: 
+            do = False
 
 print(sum)
